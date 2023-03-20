23b620c329e57ff480b5b7475632ffab85ee0aef
Tina Shi <init.defaultBranch>
2023-03-20 23:53:08 +1100

    commit tree c6a35524640c2d5215c469410c5b4bc4f1b8745b

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+
 // code your solution here
+function superbowlWin(record) {
+    let firstWin = record.find(win => win.result === "W");
+    if (firstWin !== undefined) {
+        return firstWin.year;
+    }
+    
+}
+
